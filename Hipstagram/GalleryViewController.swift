43c840f610722089a3ce205de00bcb29e1d22781
kolo13 <kori.kolodziejczak@gmail.com>
2014-10-19 00:35:51 -0700

    More code cleaning

--- Hipstagram/GalleryViewController.swift
+++ Hipstagram/GalleryViewController.swift
@@ -50,17 +50,17 @@ var flowlayout : UICollectionViewFlowLayout!
     func pinchAction(pinch : UIPinchGestureRecognizer) {
       
       
-      if pinch.state == UIGestureRecognizerState.Ended {
-        println(pinch.velocity)
-        self.collectionView.performBatchUpdates({ () -> Void in
-          if pinch.velocity > 0 {
-            self.flowlayout.itemSize = CGSize(width: self.flowlayout.itemSize.width * 2, height: self.flowlayout.itemSize.height * 2)
-          } else {
-            self.flowlayout.itemSize = CGSize(width: self.flowlayout.itemSize.width * 0.5, height: self.flowlayout.itemSize.height * 0.5)
-          }
-          }, completion: nil )
-      }
-    
+    if pinch.state == UIGestureRecognizerState.Ended {
+      println(pinch.velocity)
+      self.collectionView.performBatchUpdates({ () -> Void in
+        if pinch.velocity > 0 {
+          // if self.collectionView.size < CGSize(width: 300, height: 450){ }
+          self.flowlayout.itemSize = CGSize(width: self.flowlayout.itemSize.width * 2, height: self.flowlayout.itemSize.height * 2)
+        } else {
+          self.flowlayout.itemSize = CGSize(width: self.flowlayout.itemSize.width * 0.5, height: self.flowlayout.itemSize.height * 0.5)
+        }
+        }, completion: nil )
+    }
   }
 
   func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
